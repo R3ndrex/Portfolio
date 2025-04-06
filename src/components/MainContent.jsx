d0312fe60a4aacfb585fb182539ba64e379bff37
R3ndrex <yacholohchtoli@gmail.com>
2025-04-06 21:24:37 +0300

    move stack to its component

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,42 +1,5 @@
-import ReactImage from "../assets/react.svg";
-import ReactRouterImage from "../assets/reactrouter.svg";
-import ViteImage from "../assets/vitejs.svg";
-import VitestImage from "../assets/vitest.svg";
-import CSSImage from "../assets/css_old.svg";
-import HTMLImage from "../assets/javascript.svg";
-import JavaScriptImage from "../assets/html5.svg";
 import { Trans, useTranslation } from "react-i18next";
-
-const stack = [
-    {
-        image: HTMLImage,
-        alt: "html5-image",
-    },
-    {
-        image: CSSImage,
-        alt: "css-image",
-    },
-    {
-        image: JavaScriptImage,
-        alt: "javascript-image",
-    },
-    {
-        image: ReactImage,
-        alt: "react-image",
-    },
-    {
-        image: ReactRouterImage,
-        alt: "react--router-image",
-    },
-    {
-        image: ViteImage,
-        alt: "vite-image",
-    },
-    {
-        image: VitestImage,
-        alt: "vitest-image",
-    },
-];
+import Stack from "./Stack";
 
 export default function MainContent() {
     const { t } = useTranslation();
@@ -58,16 +21,7 @@ export default function MainContent() {
                     </p>
                 </Trans>
             </section>
-            <section>
-                <h2 className="mb-5 bg-red-300 pb-3 pt-3">{t("My stack")}</h2>
-                <ul className="flex gap-1">
-                    {stack.map(({ image, alt }) => (
-                        <li className="flex items-center" key={alt}>
-                            <img src={image} alt={alt} width="50" />
-                        </li>
-                    ))}
-                </ul>
-            </section>
+            <Stack />
         </main>
     );
 }
---
+++ src/components/Stack.jsx
@@ -0,0 +1,55 @@
+import ReactImage from "../assets/react.svg";
+import ReactRouterImage from "../assets/reactrouter.svg";
+import ViteImage from "../assets/vitejs.svg";
+import VitestImage from "../assets/vitest.svg";
+import CSSImage from "../assets/css_old.svg";
+import HTMLImage from "../assets/javascript.svg";
+import JavaScriptImage from "../assets/html5.svg";
+import { useTranslation } from "react-i18next";
+
+const stack = [
+    {
+        image: HTMLImage,
+        alt: "html5-image",
+    },
+    {
+        image: CSSImage,
+        alt: "css-image",
+    },
+    {
+        image: JavaScriptImage,
+        alt: "javascript-image",
+    },
+    {
+        image: ReactImage,
+        alt: "react-image",
+    },
+    {
+        image: ReactRouterImage,
+        alt: "react--router-image",
+    },
+    {
+        image: ViteImage,
+        alt: "vite-image",
+    },
+    {
+        image: VitestImage,
+        alt: "vitest-image",
+    },
+];
+
+export default function Stack() {
+    const { t } = useTranslation();
+    return (
+        <section>
+            <h2 className="mb-5 bg-red-300 pb-3 pt-3">{t("My stack")}</h2>
+            <ul className="flex gap-1">
+                {stack.map(({ image, alt }) => (
+                    <li className="flex items-center" key={alt}>
+                        <img src={image} alt={alt} width="50" />
+                    </li>
+                ))}
+            </ul>
+        </section>
+    );
+}
