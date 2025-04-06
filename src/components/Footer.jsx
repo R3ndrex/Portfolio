a5dc6f2792e2fcdbfa89a9eff5bab7cbb21a59d0
R3ndrex <yacholohchtoli@gmail.com>
2025-04-06 20:39:40 +0300

    Add github link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,18 @@
 import { useTranslation } from "react-i18next";
-
+import GithubImage from "../assets/github-light.svg";
 export default function Footer() {
     const { t } = useTranslation();
     return (
-        <footer className="flex justify-center mt-5">
-            {t("Lytvyneko Rostyslav")} 2025
+        <footer className="flex flex-col items-center mt-5 gap-5 bg-white">
+            <a
+                href="https://github.com/R3ndrex/Portfolio"
+                target="_blank"
+                className="flex justify-center gap-5"
+            >
+                <span className="flex items-center">Source</span>
+                <img src={GithubImage} alt="github-image" width="50" />
+            </a>
+            <div>{t("Lytvyneko Rostyslav")} 2025</div>
         </footer>
     );
 }
