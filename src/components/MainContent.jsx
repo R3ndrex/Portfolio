5cc976bf9804a3a50a7eab0240803f2bb36d815c
R3ndrex <yacholohchtoli@gmail.com>
2025-04-06 22:30:10 +0300

    add projects, make h2 bigger,,remove red color

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,12 +1,34 @@
 import { Trans, useTranslation } from "react-i18next";
 import Stack from "./Stack";
+import CVImage from "../assets/cv.png";
+import ShopppingImage from "../assets/shopping.png";
+const projects = [
+    {
+        title: "CV Generator",
+        image: CVImage,
+        alt: "cv-generator-image",
+        link: "https://github.com/R3ndrex/CV-Generator",
+        description:
+            "CV-Generator is a web application for creating and customizing your resume. It allows users to choose from pre-made templates that can be customized to suit their personal preferences.",
+    },
+    {
+        title: "Shopping Cart Simulator",
+        image: ShopppingImage,
+        alt: "shopping-site-image",
+        link: "https://github.com/R3ndrex/Shopping-Cart",
+        description:
+            "Shopping Cart Simulator is a simple and user-friendly web application for simulating online shopping. Product data is fetched from the Fake Store API, and the cart allows you to add, remove, and purchase items.",
+    },
+];
 
 export default function MainContent() {
     const { t } = useTranslation();
     return (
-        <main className="w-[60%] self-center flex flex-col gap-5 bg-white ">
+        <main className="w-[50%] self-center flex flex-col gap-5 bg-white ">
             <section>
-                <h2 className="mb-5 bg-red-300 pb-3 pt-3">{t("About Me")}</h2>
+                <h2 className="mb-5 text-5xl pb-0 mb-3 pt-3">
+                    {t("About Me")}
+                </h2>
                 <Trans i18nKey="AboutText">
                     <p>
                         I am a frontend developer with a strong foundation in
@@ -22,6 +44,28 @@ export default function MainContent() {
                 </Trans>
             </section>
             <Stack />
+            <section id="projects">
+                <h2 className="mb-5 text-5xl pb-0 mb-3 pt-3">
+                    {t("Projects")}
+                </h2>
+                <ul className="projects">
+                    {projects.map((project) => {
+                        return (
+                            <li className="bg-slate-200 rounded-2xl">
+                                <img
+                                    src={project.image}
+                                    alt={project.alt}
+                                    width={1000}
+                                />
+                                <h3 className="text-4xl p-5">
+                                    {project.title}
+                                </h3>
+                                <p className="p-5">{project.description}</p>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </section>
         </main>
     );
 }
