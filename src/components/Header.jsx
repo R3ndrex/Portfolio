2fd682e8315e573f815a9f879eb7c2c1e083ebf4
R3ndrex <yacholohchtoli@gmail.com>
2025-04-06 21:12:01 +0300

    Add border top

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const languages = [{ value: "ukr", title: "Ukrainian" }];
 export default function Header() {
     const { t, i18n } = useTranslation();
     return (
-        <header className="flex p-[1rem] items-center justify-between sticky top-0 bg-white">
+        <header className="flex border-b-1 border-slate-200 p-[1rem] items-center justify-between sticky top-0 bg-white">
             <h1>{t("Portfolio")}</h1>
             <nav className="flex gap-6">
                 <button>{t("Main Page")}</button>
