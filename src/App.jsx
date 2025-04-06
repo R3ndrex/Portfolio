2911988f768c71a45bc525c9e2179d07b5cd85a9
R3ndrex <yacholohchtoli@gmail.com>
2025-04-06 22:51:51 +0300

    Add title for page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ function App() {
     return (
         <>
             <Header />
+            <div className="mb-[5rem] text-6xl title text-center h-[100vh] bg-white flex justify-center items-center">
+                <h1>
+                    <span> Hi, Im Rostyslav Lytvynenko.</span> <br />
+                    <span className="title-profession"> Software Engineer</span>
+                </h1>
+            </div>
             <MainContent />
             <Footer />
         </>
