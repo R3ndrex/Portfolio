e6127859bf217d704c7a207e423a3085cabba0b8
R3ndrex <yacholohchtoli@gmail.com>
2025-04-06 23:10:04 +0300

    add anchors for live and source of projects,style them

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -8,6 +8,7 @@ const projects = [
         image: CVImage,
         alt: "cv-generator-image",
         link: "https://github.com/R3ndrex/CV-Generator",
+        live: "https://vercel.com/r3ndrexs-projects/cv-generator",
         description:
             "CV-Generator is a web application for creating and customizing your resume. It allows users to choose from pre-made templates that can be customized to suit their personal preferences.",
     },
@@ -16,6 +17,7 @@ const projects = [
         image: ShopppingImage,
         alt: "shopping-site-image",
         link: "https://github.com/R3ndrex/Shopping-Cart",
+        live: "https://vercel.com/r3ndrexs-projects/shopping-cart",
         description:
             "Shopping Cart Simulator is a simple and user-friendly web application for simulating online shopping. Product data is fetched from the Fake Store API, and the cart allows you to add, remove, and purchase items.",
     },
@@ -51,16 +53,28 @@ export default function MainContent() {
                 <ul className="projects flex flex-col gap-5">
                     {projects.map((project) => {
                         return (
-                            <li className="project flex flex-col w-[75%]">
-                                <img
-                                    src={project.image}
-                                    alt={project.alt}
-                                    width={800}
-                                />
-                                <h3 className="text-4xl p-5">
+                            <li className="project flex flex-col w-[70%]">
+                                <img src={project.image} alt={project.alt} />
+                                <h3 className="text-4xl p-5 pl-0">
                                     {project.title}
                                 </h3>
-                                <p className="p-5">{project.description}</p>
+                                <p className="p-5 pl-0">
+                                    {project.description}
+                                </p>
+                                <div className="flex items-center gap-5">
+                                    <a
+                                        className="text-xl bg-sky-500 text-white rounded-2xl link pb-2 pt-2 pl-4 pr-4"
+                                        href={project.link}
+                                    >
+                                        Source
+                                    </a>
+                                    <a
+                                        className="text-xl bg-sky-500 text-white rounded-2xl link pb-2 pt-2 pl-4 pr-4"
+                                        href={project.live}
+                                    >
+                                        Live
+                                    </a>
+                                </div>
                             </li>
                         );
                     })}
